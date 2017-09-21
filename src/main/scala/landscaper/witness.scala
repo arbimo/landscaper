76738634761f65de8500272a21885ad5e0727c42
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-09-21 16:42:28 +0200

    Provide a new syntax to declare literal types.

--- src/main/scala/landscaper/witness.scala
+++ src/main/scala/landscaper/witness.scala
@@ -4,13 +4,19 @@ import shapeless._
 
 object witness {
 
+  /** Type class use to declare a given type as a literal.
+    * Literal types are used to stop the recursion when inspect the content of objects.
+    */
   trait LiteralWitness[T]
   object LiteralWitness {
-    implicit object intWitness extends LiteralWitness[Int]
-    implicit object stringWitness extends LiteralWitness[String]
-    implicit object doubleWitness extends LiteralWitness[Double]
-    implicit object hnilWitness extends LiteralWitness[HNil]
-    implicit object hnilTypeWitness extends LiteralWitness[HNil.type]
-    implicit object cnilWitness extends LiteralWitness[CNil]
+    implicit val intWitness = literal[Int]
+    implicit val stringWitness = literal[String]
+    implicit val doubleWitness = literal[Double]
+    implicit val hnilWitness = literal[HNil]
+    implicit val hnilTypeWitness = literal[HNil.type]
+    implicit val cnilWitness = literal[CNil]
   }
+
+  /** Creates a new literal witness for the given type. */
+  def literal[T] = new LiteralWitness[T] {}
 }
