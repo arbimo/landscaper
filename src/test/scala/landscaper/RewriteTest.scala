74ea7e2611c6053ca234bd1bc5da5fd33e80427d
Arthur Bit-Monnot <arthur.bit-monnot@laas.fr>
2017-09-22 16:48:58 +0200

    Reorganize tests for rewrite

--- src/test/scala/landscaper/RewriteTest.scala
+++ src/test/scala/landscaper/RewriteTest.scala
@@ -1,147 +1,126 @@
 package landscaper
 
-import org.scalatest.FunSuite
+import org.scalatest.FreeSpec
 import shapeless._
 import transformations._
 
-class RewriteTest extends FunSuite {
+class RewriteTest extends FreeSpec {
 
-  val intToString = (x: Int) => x.toString
+  val intToString: Int => String    = (x: Int) => x.toString
   val toLowerCase: String => String = (x: String) => x.toLowerCase
 
-  def check[T](in: T, out: T): Unit = {
-    assert(in === out)
+  "literals" - {
+    assert(rewrite(intToString, 1) == "1")
+    assert(rewrite(intToString, "A") == "A")
+    assert(rewrite(toLowerCase, "A") == "a")
+    assert(rewrite(toLowerCase, 1) == 1)
   }
 
-  test("literals") {
-    check(
-      rewrite(intToString, 1),
-      "1"
-    )
-    check(
-      rewrite(intToString, "A"),
-      "A"
-    )
-    check(
-      rewrite(toLowerCase, "A"),
-      "a"
-    )
-    check(
-      rewrite(toLowerCase, 1),
-      1
-    )
+  "hlist" - {
+    assert(rewrite(intToString, HNil) == HNil)
+    assert(rewrite(intToString, 1 :: HNil) == "1" :: HNil)
+    assert(rewrite(toLowerCase, 1 :: "A" :: HNil) == 1 :: "a" :: HNil)
+    assert(rewrite(intToString, 1 :: "A" :: HNil) == "1" :: "A" :: HNil)
+    assert(rewrite(toLowerCase, "1" :: "A" :: HNil) == "1" :: "a" :: HNil)
+    assert(rewrite(toLowerCase, rewrite(intToString, "A" :: 1 :: HNil)) == "a" :: "1" :: HNil)
+    assert(rewrite(toLowerCase, rewrite(intToString, 1 :: "A" :: HNil)) == "1" :: "a" :: HNil)
   }
 
-  test("hlist") {
-    check(
-      rewrite(intToString, HNil),
-      HNil
-    )
-    check(
-      rewrite(intToString, 1 :: HNil),
-      "1" :: HNil
-    )
-    check(
-      rewrite(toLowerCase, 1 :: "A" :: HNil),
-      1 :: "a" :: HNil
-    )
-    check(
-      rewrite(intToString, 1 :: "A" :: HNil),
-      "1" :: "A" :: HNil
-    )
-    check(
-      rewrite(toLowerCase, "1" :: "A" :: HNil),
-      "1" :: "a" :: HNil
-    )
-    check(
-      rewrite(toLowerCase, rewrite(intToString, "A" :: 1 :: HNil)),
-      "a" :: "1" :: HNil
-    )
-    assertResult("1" :: "a" :: HNil)(
-      rewrite(toLowerCase, rewrite(intToString, 1 :: "A" :: HNil))
-    )
-  }
-
-  test("case-class") {
+  "case-class" - {
     sealed trait BC
     case class A(left: BC, right: BC)
-    case class B(s: String) extends BC
+    case class B(s: String)         extends BC
     case class C(i: Int, s: String) extends BC
-    assertCompiles("Trans[String,String,C]")
-    assertDoesNotCompile("Trans[Int, String, C]")
 
-    check(
-      rewrite(toLowerCase, C(1, "A")),
-      C(1, "a")
-    )
-    check(
-      rewrite(toLowerCase, A(B("AA"), C(1, "BB"))),
-      A(B("aa"), C(1, "bb"))
-    )
-    val f = (b: B) => C(1, b.s)
-    Trans[B,C,B :+: CNil]
-    Trans[B,C,C :+: CNil]
-    Trans[B,C, B :+: C :+: CNil]
-//    Trans[B, C, BC]
-    val x: C = rewrite(f, B(""))
-    val y: C = rewrite(f, C(1, ""))
+    "implicit resolution" - {
+      Trans[String, String, C]
+      Trans[B, C, B :+: CNil]
+      Trans[B, C, C :+: CNil]
+      Trans[B, C, B :+: C :+: CNil]
 
-    val lifted: BC => BC = { case B(s) => C(1, s) case x => x }
-    check(rewrite(lifted, B("A"): BC), C(1, "A"))
-    check(rewrite(lifted, A(B("A"), B("B"))), A(C(1, "A"), C(1, "B")))
-    check(rewrite(lifted, A(C(1, "A"), B("B"))), A(C(1, "A"), C(1, "B")))
+      assertDoesNotCompile("Trans[Int, String, C]")
+
+      // FIXME: we should be able to derive rules for this
+      // Trans[B, C, BC]
+    }
+
+    "typing" - {
+      val f    = (b: B) => C(1, b.s)
+      val x: C = rewrite(f, B(""))
+      val y: C = rewrite(f, C(1, ""))
+    }
+
+    "simple rewrites" - {
+      assert(rewrite(toLowerCase, C(1, "A")) == C(1, "a"))
+      assert(rewrite(toLowerCase, A(B("AA"), C(1, "BB"))) == A(B("aa"), C(1, "bb")))
+    }
+
+    "abstract type in nested data structures" - {
+      val lifted: BC => BC = {
+        case B(s) => C(1, s)
+        case x    => x
+      }
+      assert(rewrite(lifted, B("A"): BC) == C(1, "A"))
+      assert(rewrite(lifted, A(B("A"), B("B"))) == A(C(1, "A"), C(1, "B")))
+      assert(rewrite(lifted, A(C(1, "A"), B("B"))) == A(C(1, "A"), C(1, "B")))
+    }
+
+    "lift from specific to general type" - {
+      val g: B => B = {
+        case B(s) => B(s.toLowerCase)
+      }
+      assert(rewrite(g, A(B("A"), B("B"))) == A(B("a"), B("b")))
+      assert(rewrite(g, A(C(1, "A"), B("B"))) == A(C(1, "A"), B("b")))
+    }
   }
 
-  test("collections") {
-    check(rewrite(toLowerCase, Vector("A")), Vector("a"))
-    check(rewrite(intToString, Vector(1, 2)), Vector("1", "2"))
-    assertCompiles("val x: Vector[String] = rewrite(intToString, Vector(1))")
+  "collections" - {
+    val x: Vector[String]      = rewrite(intToString, Vector(1))
+    val y: Vector[Set[String]] = rewrite(intToString, Vector(Set(1)))
+    assert(rewrite(toLowerCase, Vector("A")) == Vector("a"))
+    assert(rewrite(intToString, Vector(1, 2)) == Vector("1", "2"))
+
     assertDoesNotCompile("val x: Set[Int] = rewrite(intToString, Set(1))")
   }
 
-  test("recursive") {
+  "recursive" - {
     case class N(opt: Option[N])
-    Trans[Int,Int,Option[Option[String]]]
+    Trans[Int, Int, Option[Option[String]]]
     assert(rewrite(toLowerCase, Some(Some("A"))) == Some(Some("a")))
 
     // FIXME: we should not require explicit type to work
-    assert(rewrite((x: Option[String]) => (None: Option[String]), Some(Some("A"): Option[String])) == Some(None))
-    Trans[Int,Int,N]
+    assert(
+      rewrite((x: Option[String]) => (None: Option[String]), Some(Some("A"): Option[String])) == Some(
+        None))
+    Trans[Int, Int, N]
   }
 
-  test("recursive ADT") {
+  "recursive ADT" - {
     sealed trait Tree
     case class Branch(left: Leaves, right: Tree) extends Tree
-    sealed trait Leaves extends Tree
-    case class Leaf(s: String) extends Leaves
-    case class LeafInt(i: Int) extends Leaves
-    assertCompiles("Generic[Tree]")
-    assertCompiles("Trans[String,String, CNil]")
-    assertCompiles("Trans[String,String, Leaf :+: CNil]")
-    assertCompiles("Trans[String,String, LeafInt :+: Leaf :+: CNil]")
-    assertCompiles("Trans[String,String, Leaves]")
-    assertCompiles("Trans[String,String, Branch]")
-    assertCompiles("Trans[String,String, Tree]")
+    sealed trait Leaves                          extends Tree
+    case class Leaf(s: String)                   extends Leaves
+    case class LeafInt(i: Int)                   extends Leaves
+    Generic[Tree]
+    Trans[String, String, CNil]
+    Trans[String, String, Leaf :+: CNil]
+    Trans[String, String, LeafInt :+: Leaf :+: CNil]
+    Trans[String, String, Leaves]
+    Trans[String, String, Branch]
+    Trans[String, String, Tree]
     val tree = Branch(Leaf("A"), Branch(Leaf("B"), Leaf("C")))
-    println(Trans[String,String,Tree])
-    check(
-      rewrite(toLowerCase, Leaf("A"): Tree),
-      Leaf("a")
-    )
-    check(
-      rewrite(toLowerCase, tree),
-      Branch(Leaf("a"), Branch(Leaf("b"), Leaf("c")))
-    )
+    println(Trans[String, String, Tree])
+    assert(rewrite(toLowerCase, Leaf("A"): Tree) == Leaf("a"))
+    assert(rewrite(toLowerCase, tree) == Branch(Leaf("a"), Branch(Leaf("b"), Leaf("c"))))
   }
 
-  test("typing") {
-    assertCompiles(
-      """
-        |val intToString = (i: Int) => i.toString
-        |val res: String = rewrite(intToString, 1)
-        |val res2: String = rewrite(intToString, "A")
-        |val res3: String :: String :: HNil = rewrite(intToString, 1 :: "A" :: HNil)
-      """.stripMargin)
+  "typing" - {
+    // simple typing rules, the following should compile
+    val intToString                    = (i: Int) => i.toString
+    val res: String                    = rewrite(intToString, 1)
+    val res2: String                   = rewrite(intToString, "A")
+    val res3: String :: String :: HNil = rewrite(intToString, 1 :: "A" :: HNil)
+
     assertDoesNotCompile("val res: Int = rewrite(intToString, 1)")
 
   }
